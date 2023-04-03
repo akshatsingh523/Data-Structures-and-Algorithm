74e71697382e54c4afd1448e10b4688a76887ef5
Akshat Singh <49590899+akshatsingh523@users.noreply.github.com>
2023-04-03 12:45:26 +0530

    Create Minimum Common Value.cpp

---
+++ Minimum Common Value.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int getCommon(vector<int>& nums1, vector<int>& nums2) {
+        vector<int> ans;
+        int i=0;
+        int j=0;
+        sort(nums1.begin(),nums1.end());
+        sort(nums2.begin(),nums2.end());
+        while(i<nums1.size() && j<nums2.size()){
+            if(nums1[i]==nums2[j]){
+                ans.push_back(nums1[i]);
+                i++;
+                j++;
+            }
+            else if(nums1[i]<nums2[j]){
+                i++;
+            }
+            else{
+                j++;
+            }
+        }
+        sort(ans.begin(),ans.end());
+        if(ans.size()>0)
+        return ans[0];
+        else{
+            return -1;
+        }
+    }
+};
