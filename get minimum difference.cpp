7f88681f106f85b838a5d165951e37b0d9f69fd9
Akshat Singh <49590899+akshatsingh523@users.noreply.github.com>
2023-06-14 13:38:08 +0530

    Create get minimum difference.cpp

---
+++ get minimum difference.cpp
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> v;
+    void inorderTraversal(TreeNode* root) {
+        if (root == nullptr) {
+            return;
+        }
+
+        inorderTraversal(root->left);
+        v.push_back(root->val);
+        inorderTraversal(root->right);
+    }
+
+    int getMinimumDifference(TreeNode* root) {
+        inorderTraversal(root);
+        int mindiff=INT_MAX;
+        for(int i=1;i<v.size();i++){
+            int diff = v[i] - v[i - 1];
+            mindiff = min(mindiff, diff);
+        }
+        return mindiff;
+    }
+};
