13e3bf49868c82e30686d3f43e5968bd0745152e
Akshat Singh <49590899+akshatsingh523@users.noreply.github.com>
2024-01-08 15:31:35 +0530

    Time: 0 ms, Memory: 6.4 MB - LeetHub

---
+++ 000n-th-tribonacci-number/000n-th-tribonacci-number.cpp
@@ -0,0 +1,19 @@
+class Solution {
+private:
+    int helper(int n, std::vector<int>& memo) {
+        if (n == 0) return 0;
+        if (n == 1 || n == 2) return 1;
+
+        if (memo[n] == -1) {
+            memo[n] = helper(n - 1, memo) + helper(n - 2, memo) + helper(n - 3, memo);
+        }
+
+        return memo[n];
+    }
+
+public:
+    int tribonacci(int n) {
+        std::vector<int> memo(n + 1, -1);
+        return helper(n, memo);
+    }
+};
